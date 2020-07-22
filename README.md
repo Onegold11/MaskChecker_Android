f9fac9ec74f79c9d41ba0041d4a36b84639a1903
Onegold11 <ujini1129@gmail.com>
2020-07-22 23:43:10 +0900

    Add README file
    + add README file
    + setting opencv init

---
+++ README.md
@@ -0,0 +1,33 @@
+Mask Checker
+============
+
+Intro
+------
+안드로이드 기기의 카메라 기능을 사용하여 마스크 착용 여부를 검사하는 앱 프로젝트입니다.
+
+Environment
+------------
++ Android Studio version: 4.0.1
++ Android version: API level 23(Marshmallow)
++ opencv version: 4.4.0
+
+Requirements
+------------
++ Android Studio environment
+  1. import module -> opencv sdk import
+
+  2. Project Structure -> Denpendencies -> app add -> module denpendencies -> add sdk
++ CMakeLists.txt setting
+  1. open cpp/CMakeLists.txt file
+
+  2. setting pathPROJECT to current project path
+
+Other open source
+-----------------
+
+Copyright
+---------
+
+Q&A
+---
+Onegold11: ujini1129@gmail.com
--- app/src/main/java/com/onegold/maskchecker/MainActivity.java
+++ app/src/main/java/com/onegold/maskchecker/MainActivity.java
@@ -1,30 +1,160 @@
 package com.onegold.maskchecker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
 
-    // Used to load the 'native-lib' library on application startup.
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements CameraBridgeViewBase.CvCameraViewListener2, AutoPermissionsListener {
+
+    private static final String TAG = "opencv";
+    private Mat matInput;
+    private Mat matResult;
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    public native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
+
+
     static {
+        System.loadLibrary("opencv_java4");
         System.loadLibrary("native-lib");
     }
+
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
+
+        AutoPermissions.Companion.loadAllPermissions(this, 1);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    /* 카메라 프리뷰 작동 시작시 호출 */
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    /* 카메라 프리뷰 작동 중지시 호출 */
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    /* 프레임 전달 필요시 호출 */
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        matInput = inputFrame.rgba();
+
+        if (matResult == null)
+
+            matResult = new Mat(matInput.rows(), matInput.cols(), matInput.type());
+
+        ConvertRGBtoGray(matInput.getNativeObjAddr(), matResult.getNativeObjAddr());
+
+        return matResult;
+    }
+
+
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(mOpenCvCameraView);
+    }
+
+    /* 사용 권한 승인 결과 */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
+    }
+
+    /* 권한 거부 */
+    @Override
+    public void onDenied(int i, String[] strings) {
+
+    }
+
+    /* 권한 승인 */
+    @Override
+    public void onGranted(int i, String[] strings) {
+        for (String permission : strings) {
+            if (permission.equals("android.permission.CAMERA")) {
+                mOpenCvCameraView.setCameraPermissionGranted();
+            }
+        }
+    }
 }
